c5730ba740b7fb2f84ee15026d7a053d7e295159
Ben Serrette <bserrett@iu.edu>
2016-12-05 15:24:32 -0500

    Sets up npm package, gitignore, and gulpfile with some basics.

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+	util = require("gulp-util"),
+	sass = require('gulp-sass'),
+	livereload = require('gulp-livereload'),
+	cssnano = require('gulp-cssnano'),
+	log = util.log;
+
+var sassFiles = "site/assets/css/*.scss";
+
+gulp.task('default', function(){
+});
+
+gulp.task('sass', function(){
+	log("Generate CSS files " + (new Date()).toString());
+	gulp.src("site/assets/css/styles.scss")
+		.pipe(sass({ style: 'expanded' }))
+		.pipe(gulp.dest("site/assets/css"))
+		;
+	log("Generated");
+});
+
+gulp.task('reload', function(){
+	setTimeout(
+		function(){gulp.src("*")
+		.pipe(livereload());
+	}, 1000);
+});
+
+gulp.task("watch", function(){
+	livereload.listen();
+	gulp.watch(["**/*.html", "**/*.twig", "php/*.php", "**/*.css", "**/*.scss", "**/*.md"], ["reload"]);
+	console.log("Watching...");
+});
+
+
+gulp.task("watchsass", function(){
+	gulp.watch(sassFiles, ["sass"]);
+	console.log("Watching CSS...");
+});
